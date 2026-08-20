568b35d85578e0599e49fb55e8bbda3d42483ab9
reethuarifh <reethuarifh@gmail.com>
2026-08-20 18:52:05 +0530

    Update and rename datatypes.js to variables.js

---
+++ variables.js
@@ -0,0 +1,13 @@
+/**Variables and how it is declared**/
+
+//var, let, const are "keywords", after keyword whatever written is known as variables technically variable is known as "identifier" , = is known as "assignment operator", whatever is assigned known as value
+
+/**Decalarations and Initialization**/
+var name; // this is known as declaration
+name = 'farhana'; // this is known as Initialization
+
+var name = 'reethu'; //it can be redeclared //
+
+
+let city = 'hyderabad'; //it cannot be redeclared //
+const country = 'india'; //it cannot be redeclared //
