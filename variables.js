42dddb089e57ef0a62c3d2d452edc170ab5bbcd5
reethuarifh <reethuarifh@gmail.com>
2026-08-20 19:26:38 +0530

    Refactor variable declarations and initializations
    Updated variable declarations and initializations in variables.js.

--- variables.js
+++ variables.js
@@ -3,8 +3,8 @@
 //var, let, const are "keywords", after keyword whatever written is known as variables technically variable is known as "identifier" , = is known as "assignment operator", whatever is assigned known as value
 
 /**Decalarations and Initialization**/
-var name; // this is known as declaration
-name = 'farhana'; // this is known as Initialization
+var myName; // this is known as declaration
+myName = 'farhana'; // this is known as Initialization
 
 var name = 'reethu'; //it can be redeclared //
 
