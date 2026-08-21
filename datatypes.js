481ddd405e1e21470c593af996a324ebebe38e65
reethuarifh <reethuarifh@gmail.com>
2026-08-21 22:54:35 +0530

    Document JavaScript datatypes in datatypes.js
    Added comments explaining different JavaScript datatypes including Primitive types like String, Number, Boolean, and Undefined.

--- datatypes.js
+++ datatypes.js
@@ -1,4 +1,31 @@
 //Different types of datatypes
 
 //1) Primitive datatypes:
+// "String" A string is a text
+const userName = 'Reethu';
+console.log(`Hello ${userName}`, typeof userName);
 
+//"Number" is used for intergers and decimals
+let age = 31;
+let rating = 4.5;
+
+const productPrice = 999;
+const productQuantity = 3;
+const total = productPrice * productQuantity;
+console.log(`Total amount is ${total}`, typeof total)
+
+//"Boolean" has only two values. True , False. Mainly it is used for decisions
+const ifLoggedIn = true;
+if(ifLoggedIn) {
+console.log("show dashboard");
+}
+
+//"Undefined" A variable exists, but value was not assigned.
+let userName;
+console.log(userName); // output: undefined
+
+let employeeDetails={
+  name: "Farhana";
+}
+console.log(employeeDetails.email); //output: undefined, because in employeeDetails we have not declared email.
+  
