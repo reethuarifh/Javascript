72de86c1a36a102a81bcf71b0a4de1959d35a0ae
reethuarifh <reethuarifh@gmail.com>
2026-08-21 13:11:44 +0530

    Refactor comments in variables.js for clarity
    Updated comments for clarity and corrected syntax.

--- variables.js
+++ variables.js
@@ -1,13 +1,19 @@
 /**Variables and how it is declared**/
 
-//var, let, const are "keywords", after keyword whatever written is known as variables technically variable is known as "identifier" , = is known as "assignment operator", whatever is assigned known as value
+//var, let, const are "keywords", after keyword whatever written is known as variables technically variable is known as "identifier" , = is known as "assignment operator", whatever is assigned known as "value".
 
 /**Decalarations and Initialization**/
 var myName; // this is known as declaration
 myName = 'farhana'; // this is known as Initialization
 
 var name = 'reethu'; //it can be redeclared //
+name = 'farhana';
+console.log(name); //output: farhana, here the value was redeclared
+
+let city = 'hyderabad'; //it cannot be redeclared , here the value can be re-assigned//
+city = 'gdk';
+console.log(city);
+//let city = 'gdk'; // here identifier with name city was already declared so again we cannot redeclared the identifier with same name city 
 
 
-let city = 'hyderabad'; //it cannot be redeclared //
 const country = 'india'; //it cannot be redeclared //
