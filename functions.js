231fe8025ec4fee7e646f1467a6f4720e5888f1d
reethuarifh <reethuarifh@gmail.com>
2026-08-23 16:38:32 +0530

    Add examples of function usage in functions.js

---
+++ functions.js
@@ -0,0 +1,58 @@
+//non-primitive datatype
+//function comes under non-primitive datatype. A function is a reusable block of code designed to perform a particular task.
+//function declaration--->function call--->function execution
+
+//funtion is a "keyword", greet is function name. just declaring a function doesn't execute it.Nothing appears in the console yet. You must call/invoke the function:
+
+//EXAMPLE:1//
+
+function greet(){
+  console.log('good morning')
+}
+greet(); //output: good morning
+
+//EXAMPLE:2//
+
+//function with parameter
+function greet(name){
+  console.log(`good morning ${name}`)
+}
+greet("Reethu"); // good morning Reethu
+
+//greet(name).()=These parentheses are where we define parameters.name is a parameter."Reethu" is a argument
+//Parameter = placeholder in function definition. Argument = actual value passed during function call.
+
+//EXAMPLE:3//
+
+//function with parameters
+function add(a,b){
+  console.log(a+b);
+}
+add(10,20);
+
+//EXAMPLE:4//
+
+//function with "return".The "return" statement stops the execution of a function and sends a specific value back to the code that called it.
+function addNumbers(a,b){
+  return a+b;// The function stops here and outputs the sum
+  console.log("This will never run"); // Unreachable code
+}
+const result= addNumbers(10,20);
+console.log(result); // output: 30
+
+//EXAMPLE:5//
+
+//funtion with "ifelse"
+
+function checkAccess(isLoggedIn){
+  if(isLoggedIn){
+    return "access success";
+  }
+  else{
+    return "access denied";
+  }
+}
+const access = checkAccess(true);
+console.log(access); // output: access success
+
+
