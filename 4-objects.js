ac729d82c72aead8c88ebfe5875fc5e8cfa6ca8e
reethuarifh <reethuarifh@gmail.com>
2026-08-27 16:11:40 +0530

    Create 4-objects.js

---
+++ 4-objects.js
@@ -0,0 +1,11 @@
+//An object is a way to store related data together using key-value pairs.
+
+const user = {
+  firstName: "Reethu",
+  lastName: "Farhana",
+  age: 31
+}
+console.log(`fullname: ${user.firstName} ${user.lastName}\nAge: ${user.age}`); //output:fullname: Reethu Farhana Age: 31
+//firstName --> key
+//"Reethu" --> value
+//{} are called object literal syntax.
