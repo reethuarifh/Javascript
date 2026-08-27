1fdd895e872ae6d24f8d27a333398c6ee3ebe0f3
reethuarifh <reethuarifh@gmail.com>
2026-08-27 15:35:09 +0530

    Clarify non-primitive data types in comments
    Updated comments to clarify non-primitive data types in JavaScript, including objects, arrays, and functions.

--- 3-nonprimitive-datatypes.js
+++ 3-nonprimitive-datatypes.js
@@ -2,15 +2,17 @@
 
 //2) Non-Primitive datatype
 
-//"Object" which stores data in key-value pairs
+//1-->" Objects ({ })" A collection of key-value pairs used to represent data structures or real-world concepts.
+
 const productDetails = {
   name: "iphone",
   price: 78000,
   inStock: true
 };
-console.log(`Name of the mobile is: ${productDetails.name}, Price is: ${productDetails.price}, In stock: ${productDetails.inStock}`);
+console.log(`Name of the mobile is: ${productDetails.name}, Price is: ${productDetails.price}, In stock: ${productDetails.inStock}`); // Name of the mobile is: iphone, Price is: 78000, In stock: true
 
-//"Array" which stores multiple values in an ordered collection. An array is used to store multiple values in a single variable.
+//2--> "Arrays ([ ])" which stores multiple values in an ordered collection. An array is used to store multiple values in a single variable.A specialized type of object used to store ordered, numbered lists of data.
+
 const skills = ["HTML","CSS","Javascript","Reactjs"]
 console.log(skills[0], skills[1]); // output: HTML, CSS
 
@@ -30,3 +32,10 @@ const products = [
 ];
 console.log(products[1]); // output: {name: "Samsung", price: 59999}
 console.log(products[0].name); // output: iphone
+
+//3--> Functions (function() {})A specialized type of object containing a block of executable code. Because they are objects, you can pass them into variables, arrays, or other functions (callbacks).
+
+const sayHello = function() {
+    return "Hi!";
+};
+console.log(sayHello()); //output: Hi!
