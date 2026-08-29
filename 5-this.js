76de1c9fd64934c0b5c5bb703f9bb188766257fc
reethuarifh <reethuarifh@gmail.com>
2026-08-29 18:06:41 +0530

    Implement UserDetails class for user message retrieval
    Added UserDetails class to manage multiple user instances with a method to get user messages.

--- 5-this.js
+++ 5-this.js
@@ -11,3 +11,39 @@ class User {
 }
 const user1 = new User("Reethu", 31, "Hyd");
 console.log(user1) // output: { message: 'Hi, I am Reethu from Hyd' }
+
+//Creating multiple users
+class UserDetails {
+    constructor(name, age, city){
+        this.name = name;
+        this.age = age;
+        this.city =  city;
+    }
+    // Adding a method right inside the class body
+    getMessage(){
+        return `Hi, I am ${this.name} from ${this.city}`;
+    }
+}
+const details1 = new UserDetails("Farhana", 31, "Hyderabad");
+const details2 = new UserDetails("Arifh", 36, "Hyderabad");
+
+console.log(details1.getMessage()) //output: Hi, I am Farhana from Hyderabad
+console.log(details2.getMessage()) //output: Hi, I am Arifh from Hyderabad
+
+------------------------------------------------------------------------------
+
+class User {
+    constructor(name, age, city) {
+        this.name = name;
+        this.age = age;
+        this.city = city;
+    }
+    getMessage() {
+        return `Hi, I am ${this.name} from ${this.city}`;
+    }
+}
+const user1 = new User("Reethu", 31, "Hyd");
+console.log(user1); //output: User { name: 'Reethu', age: 31, city: 'Hyd' }
+console.log(user1.getMessage()); //output: Hi, I am Reethu from Hyd
+
+
