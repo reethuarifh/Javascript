35401f6400d727cc6edfeb791321cb298198b17d
reethuarifh <reethuarifh@gmail.com>
2026-09-16 19:11:38 +0530

    Add examples for object and array destructuring

--- 12-destructuring.js
+++ 12-destructuring.js
@@ -30,4 +30,48 @@ const user = {
 };
 const {name, age, city="hyderabad"} = user;
 console.log(city); //output: hyderabad
+--------------------------------------------
+Example:3  you don't have to keep the same variable name.You can rename them:
+--------------------------------------------
+const user = {
+    name: "Arif",
+    age: 36
+};
+const { name: userName, age: userAge } = user;
+console.log(userName, userAge); //output: Arif 36
+------------------------------------------------------
+Example:4 Nested Object Destructuring
+------------------------------------------------------
+const user = {
+    name: "Arif",
+    address: {
+        city: "Hyderabad",
+        state: "Telangana"
+    }
+};
 
+const {name, address:{city,state}} = user;
+console.log(`My name is ${name} and Iam from ${city}`); //output:my name is Arif and Iam from Hyderabad
+
+------------------------------------------
+2. Array Destructuring : arrays don't have key names; they rely on order (index positions).
+------------------------------------------
+Example 1:
+------------------------------------------
+    
+const skills = ["HTML", "CSS", "JavaScript", "React"];
+const [first, second] = skills;
+console.log(first, second); //output: HTML CSS
+
+----------------------------------------------
+Example 2: Skipping Values
+----------------------------------------------
+    
+const skills = ["HTML", "CSS", "JavaScript", "React"];
+const [first, ,third] = skills;
+console.log(first, third); //output: HTML JavaScript
+
+
+
+
+    
