1d9c106e0b3882bac91894630898ae74c9e083db
reethuarifh <reethuarifh@gmail.com>
2026-09-16 17:51:08 +0530

    Document object destructuring with examples
    Added examples of object destructuring in JavaScript.

--- 12-destructuring.js
+++ 12-destructuring.js
@@ -1,1 +1,25 @@
+//Object and Array Destructuring is a modern JavaScript feature (introduced in ES6) that allows you to unpack values from arrays or properties from objects and assign them to distinct variables in a single, clean line of code.
+
+1. Object Destructuring
+
+Example:1
+const user = {
+    name: "Reethu",
+    age: 22,
+    city: "Hyderabad"
+};
+(Without destructuring:)
+const name = user.name;
+const age = user.age;
+const city = user.city;
+
+console.log(name); //output: Reethu
+console.log(age); //output: 22
+console.log(city); //output: Hyderabad
+
+(with destructuring)
+const {name, age, city} = user;
+console.log(name); //output: Reethu
+
+
 
